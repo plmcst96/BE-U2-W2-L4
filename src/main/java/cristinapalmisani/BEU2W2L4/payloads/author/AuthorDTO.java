01a384053a65d2503596418439530b14feefa706
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-18 16:52:13 +0100

    creazione DTO con i record per blog e autore

--- src/main/java/cristinapalmisani/BEU2W2L4/payloads/author/AuthorDTO.java
+++ src/main/java/cristinapalmisani/BEU2W2L4/payloads/author/AuthorDTO.java
@@ -1,4 +1,13 @@
 package cristinapalmisani.BEU2W2L4.payloads.author;
 
-public record AuthorDTO() {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record AuthorDTO(
+        @NotEmpty(message = "Il campo è obbligatorio")
+        @Size(max = 25, min = 3, message = "non può avere più di 25 caratteri e meno di 3 caratteri ")
+        String name,
+        String surname, String email, LocalDate birthDate, String avatar) {
 }
